5b3fb400e76e12e4440c795ea336010452bc97fd
davidjayjackson <davidjayjackson@gmail.como>
2021-02-24 16:35:48 -0500

    Created 2021-02-24

--- 20-vegetable-consumption-per-capita.R
+++ 20-vegetable-consumption-per-capita.R
@@ -11,12 +11,12 @@ vegetable_consumption %>% filter(Country == "World") %>%
   ggplot() + geom_line(aes(x=Year,y=Vegetables)) +
   scale_y_continuous(labels=comma) +
   labs(title="Worldwide Vegetable Consumption Per Capita by Year",
-       subtitle = "(1961 - 2013)",
+       subtitle = "(1961 - 2017)",
        y="Kilogram/Person/Year")
 
 ## Top Five Countreis with hight per cap consumption
 
-vegie_top <- vegetable_consumption %>% filter(Year =="2013") %>%  top_n(5,Vegetables)
+vegie_top <- vegetable_consumption %>% filter(Year =="2017") %>%  top_n(5,Vegetables)
 vegie_top <- as.data.frame(vegie_top)
 
 ggplot(vegie_top) + geom_col(aes(x=reorder(Country,Vegetables),y=Vegetables)) + coord_flip() +
@@ -24,7 +24,7 @@ ggplot(vegie_top) + geom_col(aes(x=reorder(Country,Vegetables),y=Vegetables)) + 
 
 ### Plot Bottom Five Countries by Per Capita Consumption
 
-vegie_bottom <- vegetable_consumption %>% filter(Year =="2013") %>%  top_n(-5,Vegetables)
+vegie_bottom <- vegetable_consumption %>% filter(Year =="2017") %>%  top_n(-5,Vegetables)
 vegie_bottom <- as.data.frame(vegie_bottom)
 
 ggplot(vegie_bottom) + geom_col(aes(x=reorder(Country,Vegetables),y=Vegetables)) + coord_flip() +
