655dc920b1b9442d5b580afaaecf60465ea7c1aa
davidjayjackson <davidjayjackson@gmail.como>
2021-02-25 16:35:03 -0500

    Added plots for US per capita, and top 5 countreis by per capita 2021-02-25

--- 20-vegetable-consumption-per-capita.R
+++ 20-vegetable-consumption-per-capita.R
@@ -14,6 +14,17 @@ vegetable_consumption %>% filter(Country == "World") %>%
        subtitle = "(1961 - 2017)",
        y="Kilogram/Person/Year")
 
+### US Vegetable Consumption per Capita in Kilograms
+
+vegetable_consumption %>% filter(Country == "United States") %>%
+  ggplot() + geom_line(aes(x=Year,y=Vegetables),lwd=2,col="red") +
+  scale_y_continuous(labels=comma) +
+  labs(title="United States Vegetable Consumption Per Capita by Year",
+       subtitle = "(1961 - 2017)",
+       y="Kilogram/Person/Year")
+
+
+
 ## Top Five Countries with highest per cap consumption
 
 vegie_top <- vegetable_consumption %>% filter(Year =="2017") %>%  top_n(5,Vegetables)
@@ -29,3 +40,16 @@ vegie_bottom <- as.data.frame(vegie_bottom)
 
 ggplot(vegie_bottom) + geom_col(aes(x=reorder(Country,Vegetables),y=Vegetables)) + coord_flip() +
   labs(title="Bottom Five Countries Rata by Per Capita Consumption")
+
+
+### Top Five Countries by Per Capita Vegetable consumption 
+
+countries_top_five <- vegie_top %>% select(Country) %>% left_join(vegetable_consumption,by="Country")
+head(countries_top_five)
+tail(countries_top_five)
+
+### Plots of Top Five Countries ranked by Per Capita Consumption
+
+ggplot(countries_top_five) + geom_line(aes(x=Year,y=Vegetables,col=Country)) +
+  labs(title = "Vegetables Consumption Kg/Person/Year",subtitle = "( Top 5 countries)",
+       y="Vegetables Consumed per person Kg")
